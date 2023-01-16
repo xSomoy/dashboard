84aee8c2d376b75587e287389ed0457b2e781cb6
John Deadman <xwsomoy@tutanota.com>
2023-01-17 01:41:22 +0600

    msg

--- fileOrganizer/main.py
+++ fileOrganizer/main.py
@@ -1,21 +1,40 @@
 import os
+import tkinter as tk
+from tkinter import filedialog
 
-def list_files(directory):
-    # create an empty list to store the files
-    files = []
-    # use the os.listdir() method to get a list of all files and directories in the given directory
-    for file_name in os.listdir(directory):
-        # use the os.path.join() method to construct the full file path
-        #file_path = os.path.join(directory, file_name)
-        # check if the file is a regular file (not a directory)
-        #if os.path.isfile(file_path):
-            # add the file to the list
-            files.append(file_name)
-    # return the list of files
-    return files
 
-# example usage
-files = list_files("D:\\Downloads")
+def select_directory():
+    # open a file dialog and get the selected directory
+    directory = filedialog.askdirectory()
+    # print the selected directory
+    return directory
 
-for i in files:
-    print(i)
+
+# create a tkinter window
+root = tk.Tk()
+root.title("Select Directory")
+root.geometry("300x300")
+
+# create a button to open the file dialog
+button = tk.Button(root, text="Select Directory", command=select_directory)
+button.pack()
+
+# start the tkinter event loop
+root.mainloop()
+
+
+# def list_files(directory):
+#     # create an empty list to store the files
+#     files = []
+#     # use the os.listdir() method to get a list of all files and directories in the given directory
+#     for file_name in os.listdir(directory):
+#         files.append(file_name)
+#     # return the list of files
+#     return files
+
+
+# # example usage
+# files = list_files("D:\\Downloads")
+
+# for i in files:
+#     print(i)
