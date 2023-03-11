bb1f19ea5b41df722ee246d14eeb466018163900
John Deadman <xSomoy@protonmail.com>
2023-03-11 21:17:01 +0600

    10% gathering continental data

--- narrowDownToYou/d2u.py
+++ narrowDownToYou/d2u.py
@@ -1,15 +1,36 @@
-# Real Word Datas 
+# Real Word Data
 world_population = 8021309772
+
+# AFRICA
 afria_female = 714604047
 afria_male = 718108076
 
+# ASIA
+asia_female = 2334380564
+asia_male = 2447668416
 
-male_ration = 50.42
-female_ration = 100 - 50.42
-total_male = int((world_population * male_ration) / 100 )
-total_female = int(world_population - total_male )
+# NORTH AMERICA
+north_Amerca_female = 307257554
+north_Amerca__male = 299799765
+
+# SOUTH AMERICA
+south_Amerca_female = 224509673
+south_Amerca__male = 219021362
+
+# EUROPE
+europe_female = 388955288
+europe_male = 362491426
+
+# AUSTRALIA
+australia_female = 13363462
+australia_male = 13278336
 
 
-print(total_male)
-print(total_female)
+# male_ration = 50.42
+# female_ration = 100 - 50.42
+# total_male = int((world_population * male_ration) / 100 )
+# total_female = int(world_population - total_male )
 
+
+# print(total_male)
+# print(total_female)
