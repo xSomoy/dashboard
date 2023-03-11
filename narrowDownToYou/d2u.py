b7d47580616cc807b8b34f640a1cc3b739638f0c
John Deadman <xSomoy@protonmail.com>
2023-03-11 22:05:48 +0600

    20% filter: Continent and Gender

--- narrowDownToYou/d2u.py
+++ narrowDownToYou/d2u.py
@@ -4,8 +4,8 @@
 world_population = 8043437969
 
 # AFRICA
-afria_female = 714604047
-afria_male = 718108076
+africa_female = 714604047
+africa_male = 718108076
 
 # ASIA
 asia_female = 2334380564
@@ -13,11 +13,11 @@ asia_male = 2447668416
 
 # NORTH AMERICA
 north_Amerca_female = 307257554
-north_Amerca__male = 299799765
+north_Amerca_male = 299799765
 
 # SOUTH AMERICA
 south_Amerca_female = 224509673
-south_Amerca__male = 219021362
+south_Amerca_male = 219021362
 
 # EUROPE
 europe_female = 388955288
@@ -27,16 +27,38 @@ europe_male = 362491426
 australia_female = 13363462
 australia_male = 13278336
 
-print(afria_female + afria_male + asia_female + asia_male + north_Amerca__male + north_Amerca_female + south_Amerca__male + south_Amerca_female + europe_male + europe_female + australia_female + australia_male )
+
+# UNIQUENESS FUNCTION
 
 
+continent = int(input('Which Continate you are belong? \n 1.AFRICA \n 2.ASIA \n 3.NORTH AMERICA \n 4.SOUTH AMERICA \n 5.EUROPE \n 6.AUSTRALIA\n >> '))
+gender = int(input('Which Continate you are belong? \n 1.MALE\n 2.FEMALE\n >> '))
 
 
-# male_ration = 50.42
-# female_ration = 100 - 50.42
-# total_male = int((world_population * male_ration) / 100 )
-# total_female = int(world_population - total_male )
+if continent == 1 and gender == 1:
+    unique = africa_male
+elif continent == 1 and gender ==2:
+    unique = africa_female
+elif continent == 2 and gender == 1:
+    unique = asia_male
+elif continent == 2 and gender ==2:
+    unique = asia_female
+elif continent == 3 and gender == 1:
+    unique = north_Amerca_male
+elif continent == 3 and gender ==2:
+   unique = north_Amerca_female
+elif continent == 4 and gender == 1:
+    unique = south_Amerca_male
+elif continent == 4 and gender ==2:
+    unique = south_Amerca_female
+elif continent == 5 and gender == 1:
+    unique = europe_male
+elif continent == 5 and gender ==2:
+    unique = europe_female
+elif continent == 6 and gender == 1:
+    unique = australia_male
+elif continent == 6 and gender ==2:
+    unique = australia_female
 
-
-# print(total_male)
-# print(total_female)
+print(unique)
+                
