f8b94fb2d85e382136a829d474cdaa45e1c253ec
John Deadman <xSomoy@protonmail.com>
2023-03-11 20:45:20 +0600

    narrowDown2You

---
+++ narrowDownToYou/d2u.py
@@ -0,0 +1,9 @@
+# Real Word Datas 
+world_population = 8021309772
+male_ration = 50.42
+female_ration = 100 - 50.42
+total_male = int((world_population * male_ration) / 100 )
+total_female = int(world_population - total_male )
+print(total_male)
+print(total_female)
+
